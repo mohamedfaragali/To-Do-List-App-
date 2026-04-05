f17debdacf03a95f8e6f4af0513283fb20250fd8
Mohamed Farag Ali <your_email@example.com>
2026-04-05 13:03:43 +0200

    I have finished the part of deleting the tasks

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -45,6 +45,13 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+  //delete task
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,6 +76,7 @@ class _HomePageState extends State<HomePage> {
             taskName: toDoList[index][0],
             taskCompleted: toDoList[index][1],
             onChanged: (value) => checkBoxChanged(value, index),
+            deleteFunction: (context) => deleteTask(index),
           );
         },
       ),
